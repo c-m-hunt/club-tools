194795ec7a4d57f40db57951fa4bbbfc78ad8d80
Chris Hunt <chris.hunt@hudl.com>
2020-07-22 21:02:07 +0000

    Add command line

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// produceInvoices();
+// // produceInvoices();
+
+import { program } from "commander";
+program.version("0.1.0");
+async function main() {
+    program
+        .command("availability")
+        .option("-d, --date <date>", "date for availability", "Saturday 25th July")
+        .action(async (cmd) => {
+            getAvailability(
+                process.env.AVAILABILITY_SHEET_ID,
+                process.env.AVAILABILITY_TAB_NAME
+            ).then(data => {
+                const available = data.filter(a => (a.availability.includes(cmd.date))).map(a => a.name);
+                console.log(available);
+            });
+        });
 
 
-getAvailability(
-    process.env.AVAILABILITY_SHEET_ID,
-    process.env.AVAILABILITY_TAB_NAME
-).then(data => {
-    const available = data.filter(a => (a.availability.includes("Saturday 25th July"))).map(a => a.name);
-    console.log(available);
-});
+    await program.parseAsync(process.argv);
+}
+main();
