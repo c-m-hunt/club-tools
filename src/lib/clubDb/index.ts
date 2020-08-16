589c416f726814e09904212f39594511cec24747
Chris Hunt <chris.hunt@hudl.com>
2020-08-16 21:01:44 +0100

    Add update and insert from spreadsheet

--- src/lib/clubDb/index.ts
+++ src/lib/clubDb/index.ts
@@ -5,49 +5,57 @@ import { config } from "config";
 import { getModelForClass, ReturnModelType } from "@typegoose/typegoose";
 import logger from "logger";
 
-
 let MemberModel: null | ReturnModelType<typeof Member> = null;
 export const getMembmerModel = () => {
-    if (MemberModel) {
-        return MemberModel;
-    }
-    MemberModel = getModelForClass(Member);
-    MemberModel.schema.index({ firstName: "text", lastName: "text" });
+  if (MemberModel) {
     return MemberModel;
+  }
+  MemberModel = getModelForClass(Member);
+  MemberModel.schema.index({ firstName: "text", lastName: "text" });
+  return MemberModel;
 };
 
 export const connect = async () => {
-    await mongoose.connect(config.clubDb.connectionString, { useNewUrlParser: true, useUnifiedTopology: true, dbName: config.clubDb.dbName });
+  await mongoose.connect(
+    config.clubDb.connectionString,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: config.clubDb.dbName,
+    },
+  );
 };
 
 export const insertMember = async (member: Member) => {
-    const MemberModel = getMembmerModel();
-    const createdMember = await MemberModel.create(member);
-    // logger.debug("Inserted:");
-    //logger.debug(JSON.stringify(member, null, 2));
-    //logger.debug(JSON.stringify(createdMember, null, 2));
-    return createdMember._id;
+  const MemberModel = getMembmerModel();
+  const createdMember = await MemberModel.create(member);
+  // logger.debug("Inserted:");
+  //logger.debug(JSON.stringify(member, null, 2));
+  //logger.debug(JSON.stringify(createdMember, null, 2));
+  return createdMember._id;
 };
 
 export const updateMembers = async (members: Partial<MemberDB>[]) => {
-    const MemberModel = getMembmerModel();
-    const bulk = MemberModel.collection.initializeUnorderedBulkOp();
-    for (const m of members) {
-        if (m._id) {
-            const id = m._id;
-            delete m._id;
-            bulk.find({ "_id": mongoose.Types.ObjectId(id) }).updateOne({ $set: m });
-        }
+  const MemberModel = getMembmerModel();
+  const bulk = MemberModel.collection.initializeUnorderedBulkOp();
+  for (const m of members) {
+    if (m._id) {
+      const id = m._id;
+      delete m._id;
+      bulk.find({ "_id": mongoose.Types.ObjectId(id) }).updateOne({ $set: m });
+    } else {
+      bulk.insert(m);
     }
-    const results = await bulk.execute();
-    console.log(results);
+  }
+  const results = await bulk.execute();
+  console.log(results);
 };
 
 export const getMembers = async (): Promise<any[]> => {
-    const MemberModel = getMembmerModel();
-    return await MemberModel.find({}).sort({ lastName: 1, firstName: 1 }).exec();
+  const MemberModel = getMembmerModel();
+  return await MemberModel.find({}).sort({ lastName: 1, firstName: 1 }).exec();
 };
 
 export const disconnect = async () => {
-    await mongoose.disconnect();
+  await mongoose.disconnect();
 };
--- src/lib/clubDb/types.ts
+++ src/lib/clubDb/types.ts
@@ -3,58 +3,59 @@ import { GoogleSpreadsheetRow } from "google-spreadsheet";
 import { Schema } from "mongoose";
 
 export class ExternalMapping {
-    @prop()
-    mailchimpId: string;
+  @prop()
+  mailchimpId: string;
 }
 
-
 export class Member {
-    @prop({ required: true })
-    public firstName: string;
-    @prop({ required: true })
-    lastName: string;
-    @prop()
-    phone?: string;
-    @prop()
-    email?: string;
-    @prop({ _id: false })
-    externalMapping?: ExternalMapping
-    @prop({ type: String })
-    tags: string[];
+  @prop({ required: true })
+  public firstName: string;
+  @prop({ required: true })
+  lastName: string;
+  @prop()
+  phone?: string;
+  @prop()
+  email?: string;
+  @prop({ _id: false })
+  externalMapping?: ExternalMapping;
+  @prop({ type: String })
+  tags: string[];
 }
 
 export const MemberSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    mobilePhone: String,
-    email: String,
-    externalMapping: {
-        mailchimpId: String
-    },
-    tags: [String]
+  firstName: String,
+  lastName: String,
+  mobilePhone: String,
+  email: String,
+  externalMapping: {
+    mailchimpId: String,
+  },
+  tags: [String],
 });
 
 export class MemberDB extends Member {
-    _id: string;
+  _id: string;
 }
 
 export const memberToSpreadsheetRow = (m: MemberDB) => ({
-    id: m._id,
-    firstName: m.firstName,
-    lastName: m.lastName,
-    phone: m.phone,
-    email: m.email,
-    tags: m.tags?.join(",")
+  id: m._id,
+  firstName: m.firstName,
+  lastName: m.lastName,
+  phone: m.phone,
+  email: m.email,
+  tags: m.tags?.join(","),
 });
 
-export const spreadsheetRowToMember = (row: GoogleSpreadsheetRow): Partial<MemberDB> => {
-    const tagString = row._rawData[4] as string;
-    return {
-        _id: row._rawData[0] as string,
-        firstName: row._rawData[1] as string,
-        lastName: row._rawData[2] as string,
-        phone: row._rawData[3] as string,
-        email: row._rawData[4] as string,
-        tags: tagString ? tagString.split(",") : []
-    };
+export const spreadsheetRowToMember = (
+  row: GoogleSpreadsheetRow,
+): Partial<MemberDB> => {
+  const tagString = row._rawData[4] as string;
+  return {
+    _id: row._rawData[0] as string,
+    firstName: row._rawData[1] as string,
+    lastName: row._rawData[2] as string,
+    phone: row._rawData[3] as string,
+    email: row._rawData[4] as string,
+    tags: tagString ? tagString.split(",") : [],
+  };
 };
